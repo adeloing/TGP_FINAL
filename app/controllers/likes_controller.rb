dbbf815b61c46c5237f1135775d22f3752ed989d
adeloing <adeloing@gmail.com>
2022-11-04 13:00:28 +0100

    ok

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,16 +6,12 @@ class LikesController < ApplicationController
       # Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
     end
 
-    def show
-      # Méthode qui récupère le potin concerné et l'envoie à la view show (show.html.erb) pour affichage
-    end
-
     def new
-      # Méthode qui crée un potin vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
+      @like = Like.new
     end
 
     def create
-        @like = Like.new('gossip_id' => params[:gossip_id], user: User.find_by(id: session[:user_id]))
+        @like = Like.new(gossip: Gossip.find_by(id: session[:gossip_id]), user: User.find_by(id: session[:user_id]))
         if @like.save
           flash[:success] = "like given !"
           redirect_to '/'
@@ -23,16 +19,6 @@ class LikesController < ApplicationController
           flash[:notice] = "like not given !"
           render '/gossips'
         end
-    end
-
-    def edit
-      # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
-    end
-
-    def update
-      # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
-      # pour info, le contenu de ce formulaire sera accessible dans le hash params
-      # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
     end
 
     def destroy
